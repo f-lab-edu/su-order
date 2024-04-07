f65c0603abbd837cfc4bf79f12774a84fd1f0c51
kimsuyeondev <rhaehl@naver.com>
2024-04-07 16:38:16 +0900

    feat: Hello world log add

--- src/main/java/com/example/demo/SuOrderApplication.java
+++ src/main/java/com/example/demo/SuOrderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SuOrderApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(SuOrderApplication.class, args);
 	}
 
